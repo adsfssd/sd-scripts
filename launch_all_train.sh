bbd0643d625e011480b4529d4da4da8b8d4643e2
bluvoll <gordigo24@hotmail.com>
2025-11-28 18:54:15 -0500

    fix use_sga in optimizer_utils

--- launch_all_train.sh
+++ launch_all_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-accelerate launch sdxl_train.py --dataset_config="dataset1.toml" --config_file="config1.toml" --flow_model  --flow_use_ot  --flow_timestep_distribution logit_normal --flow_uniform_static_ratio 2.5 --vae_custom_scale 0.1280 --vae_custom_shift 0.1726 --use_zero_cond=True  --vae_batch_size 6 --vae_reflection_padding --deepspeed  --log_caption_dropout  --fused_optimizer_groups 20
+accelerate launch sdxl_train_network.py --dataset_config="dataset1.toml" --config_file="config-example-LoCON.toml" --flow_model  --flow_use_ot  --flow_timestep_distribution uniform --flow_uniform_static_ratio 2.5 --vae_custom_scale 0.1280 --vae_custom_shift 0.1726 --use_zero_cond=True  --vae_batch_size 6 --vae_reflection_padding --use_zero_cond_dropout=True --use_sga
 echo "All training jobs finished. Press any key to close..."
 read -n 1 -s -r -p ""
